45a274cc303ac6e3f61111b724715b7adc3fd96c
nelsenm25 <167141531+nelsenm25@users.noreply.github.com>
2025-01-24 18:50:04 -0600

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://fxycdpfulnyolxemlvrl.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4eWNkcGZ1bG55b2x4ZW1sdnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3NjU4MDgsImV4cCI6MjA1MzM0MTgwOH0.YNIBOFu-3MsS5psVpTNNIjF2HW9qwAWn3b8qCJrnILM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
