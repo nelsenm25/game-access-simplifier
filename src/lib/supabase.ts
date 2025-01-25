c8133f8d4c8a66b0b942134fe352db1b9d0862f9
nelsenm25 <167141531+nelsenm25@users.noreply.github.com>
2025-01-24 19:23:52 -0600

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASEURL = 'https://xyzcompany.supabase.co';
-const SUPABASEANONKEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // Your anon key here
+const SUPABASEURL = 'SUPABASEURL';
+const SUPABASEANONKEY = 'SUPABASEANONKEY'; // Your anon key here
 
 export const supabase = createClient(SUPABASEURL, SUPABASEANONKEY);
